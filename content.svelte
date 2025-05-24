61bdedff3199f3efcfd084cd3ee6904f7af87f60
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-05-24 23:23:27 +0200

    delete file

---
+++ content.svelte
@@ -0,0 +1,17 @@
+<!-- Intro/Hero -->
+<h1>
+  Hi, I'm Nazneen Sheikkariem a passionate front-end developer with an eye for
+  detail and a love for creating user-friendly websites.
+</h1>
+
+<!-- About me -->
+<h2>About me</h2>
+
+<!-- Projects -->
+<h2>My projects</h2>
+
+<!-- Contact -->
+<h2>Let’s build something great together.</h2>
+<p>Reach out via the form or email, I’d love to hear from you.</p>
+
+
