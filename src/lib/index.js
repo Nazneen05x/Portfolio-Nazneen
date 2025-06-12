de5b6411382b15eb82ac1c7a1c0ed705dbb031fa
Nazneen <112861261+Nazneen05x@users.noreply.github.com>
2025-06-12 04:29:13 +0200

    fix: typo

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,7 +5,7 @@ export { default as Heroheader } from "$lib/components/Heroheader/heroHeader.sve
 export { default as Marquee } from "$lib/components/Animations/marquee.svelte";
 
 // About me
-export { default as AboutMe } from "$lib/components/AboutMe/aboutMe.svelte";
+export { default as AboutMe } from "$lib/components/Aboutme/aboutMe.svelte";
 
 // My projects
 export { default as Projects } from "$lib/components/Projects/project.svelte";
